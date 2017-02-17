c95cd646813b78526253a03cea18318e35493305
abaojin <abaojin@gmail.com>
2017-02-17 11:25:52 +0800

    fix call rule
    Signed-off-by: abaojin <abaojin@gmail.com>

--- PluginsDeme/Assets/TstUnity/PluginUtils.cs
+++ PluginsDeme/Assets/TstUnity/PluginUtils.cs
@@ -13,15 +13,15 @@ public static class PluginUtils
         private const string UNITYDLL = "tstunity";
 #endif
 
-    [DllImport(UNITYDLL)]
+    [DllImport(UNITYDLL, CallingConvention = CallingConvention.Cdecl)]
     public static extern int tst_add(int a, int b);
 
-    [DllImport(UNITYDLL)]
+    [DllImport(UNITYDLL, CallingConvention = CallingConvention.Cdecl)]
     public static extern int tst_sub(int a, int b);
 
-    [DllImport(UNITYDLL)]
+    [DllImport(UNITYDLL, CallingConvention = CallingConvention.Cdecl)]
     public static extern int tst_div(int a, int b);
 
-    [DllImport(UNITYDLL)]
+    [DllImport(UNITYDLL, CallingConvention = CallingConvention.Cdecl)]
     public static extern int tst_mul(int a, int b);
 }
