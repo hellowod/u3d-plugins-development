440fab26a10cdef71011746adc008d941216b409
abaojin <abaojin@gmail.com>
2017-02-23 11:00:28 +0800

    updaete
    Signed-off-by: abaojin <abaojin@gmail.com>

--- NativePlugins/tstunity/tstunity_conf.h
+++ NativePlugins/tstunity/tstunity_conf.h
@@ -2,8 +2,12 @@
 config
 */
 
-#if defined(_MSC_VER)
-#define TST_API __declspec(dllexport)
+#if defined _WIN32
+    #if LIBTST_BUILD
+        #define TST_API __declspec(dllexport)
+    #else
+        #define TST_API __declspec(dllimport)
+    #endif
 #else
-#define TST_API
+    #define TST_API
 #endif
