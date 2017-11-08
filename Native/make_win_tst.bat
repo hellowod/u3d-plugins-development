ed6e2c5996b3b79f4890192189b959f308e756e6
abaojin <abaojin@gmail.com>
2017-11-08 10:57:44 +0800

    编译
    Signed-off-by: abaojin <abaojin@gmail.com>

---
+++ Native/make_win_tst.bat
@@ -0,0 +1,17 @@
+echo off
+
+mkdir build32 & pushd build32
+cmake -G"Visual Studio 15 2017" ..
+popd
+cmake --build build32 --config Release
+md ..\Bin\Native\x86
+copy /Y build32\Release\tstunity.dll ..\Bin\Native\x86\tstunity.dll
+
+mkdir build64 & pushd build64
+cmake -G"Visual Studio 15 2017 Win64" ..
+popd
+cmake --build build64 --config Release
+md ..\Bin\Native\x86_64
+copy /Y build64\Release\tstunity.dll ..\Bin\Native\x86_64\tstunity.dll
+
+pause
