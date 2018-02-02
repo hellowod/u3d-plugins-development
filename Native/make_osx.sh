03517414541eb4c17e9a37192a9631d2c8ae6d2b
abaojin <abaojin@gmail.com>
2018-02-02 11:54:34 +0800

    plugins move src
    Signed-off-by: abaojin <abaojin@gmail.com>

--- Native/make_osx.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build_osx && cd build_osx
-cmake -GXcode ../
-cd ..
-cmake --build build_osx --config Release
-mkdir -p ../Build/Native/tstunity.bundle/Contents/MacOS/
-cp build_osx/Release/tstunity.bundle/Contents/MacOS/tstunity ../Build/Native/tstunity.bundle/Contents/MacOS/tstunity
-
--- Src/Native/make_ios.sh
+++ Src/Native/make_ios.sh
@@ -2,6 +2,6 @@ mkdir -p build_ios && cd build_ios
 cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/iOS.toolchain.cmake  -GXcode ../
 cd ..
 cmake --build build_ios --config Release
-mkdir -p ../Build/Native/iOS/
-cp build_ios/Release-iphoneos/libtstunity.a ../Build/Native/iOS/libtstunity.a
+mkdir -p ../../Build/Native/iOS/
+cp build_ios/Release-iphoneos/libtstunity.a ../../Build/Native/iOS/libtstunity.a
 
