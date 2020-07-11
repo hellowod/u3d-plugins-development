01e44e01b08b5b71ac0dd85e33ca2213fb08211b
abaojin <abaojin@gmail.com>
2020-07-11 17:16:36 +0800

    update plugin

---
+++ example/Assets/Scripts/NativeDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NativeDemo : MonoBehaviour
+{
+    private void CallCDemo()
+    {
+        SceneManager.LoadScene("C");
+    }
+
+    private void CallSystemDemo()
+    {
+        SceneManager.LoadScene("System");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+
+        if (GUILayout.Button("Demo C", GUILayout.Height(Screen.height >> 1)))
+        {
+            CallCDemo();
+        }
+
+        if (GUILayout.Button("Demo System", GUILayout.Height(Screen.height >> 1)))
+        {
+            CallSystemDemo();
+        }
+
+        GUILayout.EndArea();
+    }
+}
