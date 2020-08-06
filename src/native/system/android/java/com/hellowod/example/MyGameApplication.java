cae876b8cb6a6f1dd14b8222d8d4aae64314583f
abaojin <abaojin@gmail.com>
2020-08-06 19:18:31 +0800

    android update
    Signed-off-by: abaojin <abaojin@gmail.com>

--- src/native/system/android/java/com/hellowod/example/MyGameApplication.java
+++ src/native/system/android/java/com/hellowod/example/MyGameApplication.java
@@ -4,10 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 public class MyGameApplication extends Application {
+	private static String TAG = "MyGameActivity";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(AppConst.TAG, "MyGameApplication.onCreate");
+        Log.d(TAG, "MyGameApplication.onCreate");
     }
 }
